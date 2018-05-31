517b5d6103f7e93b2d823d014793996a09a0b32e
jayesh.hinge <jayesh.hinge@192.168.151.109>
2018-05-31 10:24:33 +0530

    c1

---
+++ Testing/src/com/test/Demo1.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Demo1 {
+	
+	public static void main(String[] arg) {
+		List<String> list=new ArrayList<String>();
+		list.add("test1");
+		list.add("test2");
+		list.add("test3");
+		list.add("test4");
+		for (String s:list) {
+			System.out.print(s+", ");
+		}
+		
+	}
+	
+	
+}
