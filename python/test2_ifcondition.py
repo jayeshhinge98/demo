da44e3a9e4c153c13ae58188cd00443c01f9f8df
jayeshhinge98 <jayeshhinge98@gmail.com>
2018-06-07 17:14:03 +0530

    added python files

---
+++ python/test2_ifcondition.py
@@ -0,0 +1,11 @@
+n=int(input("Enter number?"))
+name=input("Enter name?")
+if n<0:
+    if name=="sam":
+        print("both condition mate")
+    else:
+        print("second condition failed")
+    print("single condition mate")
+else:
+    print("Absolute value of ",n," is ",n)
+    print("Sum of two number is",20+450)
