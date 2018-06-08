94281dc95856166d12cbf8971c3575f9116fb967
jayeshhinge98 <jayeshhinge98@gmail.com>
2018-06-08 17:16:37 +0530

    Updated SuiteBase with all drivers

--- DemoTest/src/main/java/utilities/SuiteBase.java
+++ DemoTest/src/main/java/utilities/SuiteBase.java
@@ -1,45 +1,78 @@
 package utilities;
 
-import java.io.IOException;
-import java.net.URL;
+import java.net.MalformedURLException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import io.appium.java_client.ios.IOSDriver;
-
 public class SuiteBase {
-	public static WebDriver driver=null;
-	public static IOSDriver iOSdriver=null;
-	
+	public static WebDriver chdriver = null;
+	public static WebDriver iOSdriver = null;
+	public static WebDriver safaridriver = null;
+	public static WebDriver ffdriver = null;
+	public static HtmlUnitDriver hldriver = null;
+	//public static ExtentReport report=null;
+
 	@BeforeTest
-	public void setUp() throws IOException {
-		System.setProperty("webdriver.gecko.driver","path to geckodriver.exe");
-		System.setProperty("webdriver.chrome.driver","path to chromedriver.exe");
-		driver=new ChromeDriver();
-	//	driver=new FirefoxDriver();
-		// set up appium
+	public void setUp(){
+		//setUpSafari();	
+		//safaridriver.get("https://www.google.com");
+		// setUpChrome();
+		// chdriver.get("https://www.google.com");
+		setUpHeadLessBrowser();
+		hldriver.get("https://www.google.com");
+		
+	}
+
+	public void setUpSafari() {
+		safaridriver = new SafariDriver();
+		safaridriver.manage().window().maximize();
+	}
+	public void setUpHeadLessBrowser() {
+		hldriver = new HtmlUnitDriver();
+		hldriver.setJavascriptEnabled(true);
+		hldriver.manage().window().maximize();
+	}
+
+	public void setUpChrome() {
+		System.setProperty("webdriver.chrome.driver", "/Users/jayesh.hinge/git/demo/DemoTest/src/main/resources/chromedriver");
+		chdriver = new ChromeDriver();
+		chdriver.manage().window().maximize();
+	}
+
+	public void setUpFirefox() {
+		System.setProperty("webdriver.gecko.driver", "/src/main/resources/geckodriver");
+		ffdriver = new FirefoxDriver();
+		ffdriver.manage().window().maximize();
+	}
+
+	public void setUpIOS() throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "iOS");
 		capabilities.setCapability(CapabilityType.VERSION, "6.1");
-		capabilities.setCapability(CapabilityType.PLATFORM_NAME, "Mac");
-		capabilities.setCapability("app","/Users/username/Downloads/InternationalMountains   /build/Release-iphonesimulator/InternationalMountains.app");
-		iOSdriver = new IOSDriver(new URL("http://127.0.0.1:4725/wd/hub"), capabilities);
-		//Desktop.getDesktop().open(new File("/Applications/Appium.app"));
-
+		//capabilities.setCapability(CapabilityType.PLATFORM_NAME, "Mac");
+		capabilities.setCapability("app",
+				"/Users/username/Downloads/InternationalMountains   /build/Release-iphonesimulator/InternationalMountains.app");
+		// iOSdriver = new IOSDriver(new URL("http://127.0.0.1:4725/wd/hub"),
+		// capabilities);
+		// Desktop.getDesktop().open(new File("/Applications/Appium.app"));
 	}
-	
-	@AfterTest(alwaysRun=true)
+
+	@AfterTest(alwaysRun = true)
 	public void cleanUp() {
-		driver.quit();
-		//driver.quit();
-		iOSdriver.quit();
-//		iOSdriver.quit();	
+		 //chdriver.quit();
+		// ffdriver.quit();
+		//safaridriver.quit();
+		// iOSdriver.quit();
+		// iOSdriver.quit();
+		 hldriver.quit();
 	}
-	
-}
 
+}
--- DemoTest/src/test/java/com/test/demo1.java
+++ DemoTest/src/test/java/com/test/demo1.java
@@ -1,16 +1,22 @@
 package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import utilities.TestUtitlies;
+import utilities.SuiteBase;
 
-public class demo1 extends TestUtitlies {
+public class demo1 extends SuiteBase {
 
 	@Test
-	public void login_test(){
-		//randomString(5);
-		System.out.println("Random=>"+randomAlphabetic(20));
-		System.out.println("Random=>"+randomNumeric(20));
-		//git change 1
-
+	public void testwithHeadLessBrowser() {
+		System.out.println("Browser loaded, title is:=>"+hldriver.getTitle());
+		Assert.assertEquals(hldriver.getTitle(), "Google","Title doesn't match");
+		hldriver.findElement(By.xpath("//input[@title='Search']")).sendKeys("selenium");;
+		hldriver.findElement(By.xpath("//input[@title='Search']")).sendKeys(Keys.ENTER);;
+		System.out.println("Browser loaded, title is:=>"+hldriver.getTitle());
+		Assert.assertEquals(hldriver.getTitle(), "selenium - Google Search","Title doesn't match");		
 	}
+	
 }
